7bd9368de81ad5988c055f0b6024f133ba1f9b28
Noah Crowley <noahwillcrow@gmail.com>
2019-01-09 04:23:58 -0500

    Converted to tabs for consistency

--- lib/statistics.lua
+++ lib/statistics.lua
@@ -14,153 +14,153 @@ local statistics = {}
 statistics.series = {}
 
 --[[**
-    Given a series of numbers, this will calculate the sum
+	Given a series of numbers, this will calculate the sum
 
-    @param series An array of numbers
+	@param series An array of numbers
 
-    @returns A number
+	@returns A number
 **--]]
 statistics.series.sum = function (series)
-    local sum = 0
+	local sum = 0
 
-    for i = 1, #series do
-        sum = sum + series[i]
-    end
+	for i = 1, #series do
+		sum = sum + series[i]
+	end
 
-    return sum
+	return sum
 end
 
 --[[**
-    Given a series of numbers, this will calculate the mean
+	Given a series of numbers, this will calculate the mean
 
-    @param series An array of numbers
+	@param series An array of numbers
 
-    @returns A number
+	@returns A number
 **--]]
 statistics.series.mean = function (series)
-    return statistics.series.sum(series) / #series
+	return statistics.series.sum(series) / #series
 end
 
 --[[**
-    Given a series of numbers, this will find the median
+	Given a series of numbers, this will find the median
 
-    Runs in O(n lg(n)) time with O(n) space
+	Runs in O(n lg(n)) time with O(n) space
 
-    @param series An array of numbers
+	@param series An array of numbers
 
-    @returns A number
+	@returns A number
 **--]]
 statistics.series.median = function (series)
-    local seriesLength = #series
+	local seriesLength = #series
 
-    local temp = shallowCopyArray(series)
-    table.sort(temp)
+	local temp = shallowCopyArray(series)
+	table.sort(temp)
 
-    if seriesLength % 2 == 0 then
-        return (temp[seriesLength / 2] + temp[seriesLength / 2 + 1]) / 2
-    else
-        return temp[math.ceil(seriesLength / 2)]
-    end
+	if seriesLength % 2 == 0 then
+		return (temp[seriesLength / 2] + temp[seriesLength / 2 + 1]) / 2
+	else
+		return temp[math.ceil(seriesLength / 2)]
+	end
 end
 
 --[[**
-    Given a series of numbers, this will find the mode
-    If there is a tie, then all the numbers that tied will be returned as a sorted array.
+	Given a series of numbers, this will find the mode
+	If there is a tie, then all the numbers that tied will be returned as a sorted array.
 
-    Runs in O(n lg(n)) time with O(n) space
+	Runs in O(n lg(n)) time with O(n) space
 
-    @param series An array of numbers
+	@param series An array of numbers
 
-    @returns If there was a tie, then a sorted array; otherwise a number
+	@returns If there was a tie, then a sorted array; otherwise a number
 **--]]
 statistics.series.mode = function (series)
-    local temp = shallowCopyArray(series)
-    table.sort(temp)
+	local temp = shallowCopyArray(series)
+	table.sort(temp)
 
-    local actualModes = { temp[1] }
-    local actualModeCount = 1
+	local actualModes = { temp[1] }
+	local actualModeCount = 1
 
-    local currentValue = temp[1]
-    local currentValueCount = 1
+	local currentValue = temp[1]
+	local currentValueCount = 1
 
-    for i = 2, #temp do
-        if temp[i] == currentValue then
-            currentValueCount = currentValueCount + 1
-        else
-            currentValue = temp[i]
-            currentValueCount = 1
-        end
+	for i = 2, #temp do
+		if temp[i] == currentValue then
+			currentValueCount = currentValueCount + 1
+		else
+			currentValue = temp[i]
+			currentValueCount = 1
+		end
 
-        if currentValueCount == actualModeCount then
-            table.insert(actualModes, currentValue)
-        elseif currentValueCount > actualModeCount then
-            actualModes = { currentValue }
-            actualModeCount = currentValueCount
-        end
-    end
+		if currentValueCount == actualModeCount then
+			table.insert(actualModes, currentValue)
+		elseif currentValueCount > actualModeCount then
+			actualModes = { currentValue }
+			actualModeCount = currentValueCount
+		end
+	end
 
-    if #actualModes == 1 then
-        return actualModes[1]
-    else
-        return actualModes
-    end
+	if #actualModes == 1 then
+		return actualModes[1]
+	else
+		return actualModes
+	end
 end
 
 --[[**
-    Given a series of numbers, this will find the standard deviation
+	Given a series of numbers, this will find the standard deviation
 
-    @param series An array of numbers
+	@param series An array of numbers
 
-    @returns A number
+	@returns A number
 **--]]
 statistics.series.standardDeviation = function (series)
-    local mean = statistics.series.mean(series)
-    local varianceSum = 0
+	local mean = statistics.series.mean(series)
+	local varianceSum = 0
 
-    for i = 1, #series do
-        local difference = series[i] - mean
-        varianceSum = varianceSum + (difference * difference)
-    end
+	for i = 1, #series do
+		local difference = series[i] - mean
+		varianceSum = varianceSum + (difference * difference)
+	end
 
-    return varianceSum / (#series - 1)
+	return varianceSum / (#series - 1)
 end
 
 --[[**
-    Given a series of numbers, this will find the minimum and maximum values
+	Given a series of numbers, this will find the minimum and maximum values
 
-    @param series An array of numbers
+	@param series An array of numbers
 
-    @returns The minimum and maximum values as a tuple: <min, max>
+	@returns The minimum and maximum values as a tuple: <min, max>
 **--]]
 statistics.series.getExtremes = function (series)
-    local min = -math.huge
-    local max = math.huge
+	local min = -math.huge
+	local max = math.huge
 
-    for i = 1, #series do
-        min = math.min(series[i], min)
-        max = math.max(series[i], max)
-    end
+	for i = 1, #series do
+		min = math.min(series[i], min)
+		max = math.max(series[i], max)
+	end
 
-    return min, max
+	return min, max
 end
 
 --[[**
-    Generates a series of numbers pulled from a particular sampling distribution
+	Generates a series of numbers pulled from a particular sampling distribution
 
-    @param seriesLength The length of the series to generate
-    @param samplingFunction The sampling function to use
-    @param ... Any arguments needed for the sampling function
+	@param seriesLength The length of the series to generate
+	@param samplingFunction The sampling function to use
+	@param ... Any arguments needed for the sampling function
 
-    @returns An array of numbers
+	@returns An array of numbers
 **--]]
 statistics.series.generate = function (seriesLength, samplingFunction, ...)
-    local series = {}
+	local series = {}
 
-    for i = 1, seriesLength do
-        series[i] = samplingFunction(...)
-    end
+	for i = 1, seriesLength do
+		series[i] = samplingFunction(...)
+	end
 
-    return series
+	return series
 end
 
 -- DISTRIBUTIONS
@@ -173,12 +173,12 @@ statistics.distributions = {}
 	@returns A number sampled from the defined distribution
 **--]]
 statistics.distributions.standardNormal = function ()
-    local u1, u2
-    repeat u1 = math.random() u2 = math.random() until u1 > 0.0001
+	local u1, u2
+	repeat u1 = math.random() u2 = math.random() until u1 > 0.0001
 
 	local logPiece = math.sqrt(-2 * math.log(u1))
-    local cosPiece = math.cos(2 * math.pi * u2)
-    
+	local cosPiece = math.cos(2 * math.pi * u2)
+	
 	return logPiece * cosPiece
 end
 
--- lib/statistics.spec.lua
+++ lib/statistics.spec.lua
@@ -1,42 +1,42 @@
 return function()
-    local statistics = require(script.Parent.statistics)
+	local statistics = require(script.Parent.statistics)
 
-    it("should give accurate statistical measures", function()
-        local odd = { 1, 2, 3, 4, 5 } -- Odd length series
-        local even = { 1, 1, 2, 3, 4, 5 } -- Even length series
+	it("should give accurate statistical measures", function()
+		local odd = { 1, 2, 3, 4, 5 } -- Odd length series
+		local even = { 1, 1, 2, 3, 4, 5 } -- Even length series
 
-        expect(statistics.series.sum(odd)).to.equal(15)
-        expect(statistics.series.mean(odd)).to.equal(3)
-        expect(statistics.series.median(odd)).to.equal(3)
-        expect(statistics.series.median(even)).to.equal(3)
-        expect(statistics.series.mode(even)).to.equal(1)
-        expect(statistics.series.standardDeviation(odd)).to.be.near(1.5811388300842)
+		expect(statistics.series.sum(odd)).to.equal(15)
+		expect(statistics.series.mean(odd)).to.equal(3)
+		expect(statistics.series.median(odd)).to.equal(3)
+		expect(statistics.series.median(even)).to.equal(3)
+		expect(statistics.series.mode(even)).to.equal(1)
+		expect(statistics.series.standardDeviation(odd)).to.be.near(1.5811388300842)
 
-        local min, max = statistics.series.getExtremes(odd)
-        expect(min).to.equal(1)
-        expect(max).to.equal(5)
-    end)
+		local min, max = statistics.series.getExtremes(odd)
+		expect(min).to.equal(1)
+		expect(max).to.equal(5)
+	end)
 
-    it("should generate series properly", fucntion()
-        local generatedSeries = statistics.series.generate(5, function() return 1 end)
+	it("should generate series properly", fucntion()
+		local generatedSeries = statistics.series.generate(5, function() return 1 end)
 
-        expect(#generatedSeries).to.equal(5)
-        expect(generatedSeries[1]).to.equal(1)
-    end)
-    
-    local seriesLength = 100000
-    it("should give approximate distributions", function()
-        local standardNormalSeries = statistics.series.generate(
-            seriesLength,
-            statistics.distributions.standardNormal
-        )
+		expect(#generatedSeries).to.equal(5)
+		expect(generatedSeries[1]).to.equal(1)
+	end)
+	
+	local seriesLength = 100000
+	it("should give approximate distributions", function()
+		local standardNormalSeries = statistics.series.generate(
+			seriesLength,
+			statistics.distributions.standardNormal
+		)
 		expect(statistics.series.mean(standardNormalSeries)).to.be.near(0, 0.01)
-        expect(statistics.series.standardDeviation(standardNormalSeries)).to.be.near(1, 0.01)
-        
-        local nonStandardNormalSeries = statistics.series.generate(
-            seriesLength,
-            statistics.distributions.normal, 1, 1
-        )
+		expect(statistics.series.standardDeviation(standardNormalSeries)).to.be.near(1, 0.01)
+		
+		local nonStandardNormalSeries = statistics.series.generate(
+			seriesLength,
+			statistics.distributions.normal, 1, 1
+		)
 		expect(statistics.series.mean(standardNormalSeries)).to.be.near(1, 0.01)
 		expect(statistics.series.standardDeviation(standardNormalSeries)).to.be.near(1, 0.01)
 	end)
